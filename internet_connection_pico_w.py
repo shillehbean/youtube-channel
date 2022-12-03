5b6f04d6b88ddf7b293db121e6423bc5b992574b
Mahmood Mustafa Youssef Shilleh <104477522+shillehbean@users.noreply.github.com>
2022-12-02 19:53:14 -0600

    Create internet_connection_pico_w.py

---
+++ internet_connection_pico_w.py
@@ -0,0 +1,27 @@
+import time
+import network
+
+def connect_to_internet(ssid, password):
+    # Pass in string arguments for ssid and password
+    
+    # Just making our internet connection
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+    
+    # Wait for connect or fail
+    max_wait = 10
+    while max_wait > 0:
+      if wlan.status() < 0 or wlan.status() >= 3:
+        break
+      max_wait -= 1
+      print('waiting for connection...')
+      time.sleep(1)
+    # Handle connection error
+    if wlan.status() != 3:
+       raise RuntimeError('network connection failed')
+    else:
+      print('connected')
+      status = wlan.ifconfig()
+      
+connect_to_internet('<your internet name>', '<your internet password>')
