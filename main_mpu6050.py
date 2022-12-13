82b1f994da0b9fd3f760540a902c244fcbc5da5d
Mahmood Mustafa Youssef Shilleh <104477522+shillehbean@users.noreply.github.com>
2022-12-12 23:15:36 -0600

    Create main_mpu6050.py

---
+++ main_mpu6050.py
@@ -0,0 +1,21 @@
+#Shows Pi is on by turning on LED when plugged in
+LED = machine.Pin("LED", machine.Pin.OUT)
+LED.on()
+
+from imu import MPU6050
+from time import sleep
+from machine import Pin, I2C
+
+i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
+imu = MPU6050(i2c)
+
+while True:
+    ax=round(imu.accel.x,2)
+    ay=round(imu.accel.y,2)
+    az=round(imu.accel.z,2)
+    gx=round(imu.gyro.x)
+    gy=round(imu.gyro.y)
+    gz=round(imu.gyro.z)
+    tem=round(imu.temperature,2)
+    print("ax",ax,"\t","ay",ay,"\t","az",az,"\t","gx",gx,"\t","gy",gy,"\t","gz",gz,"\t","Temperature",tem,"        ",end="\r")
+    sleep(0.2) 
