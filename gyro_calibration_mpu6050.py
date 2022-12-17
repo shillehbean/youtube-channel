e3ace6146c430ed0e1badfa82c8deed8f8f1b642
Mahmood Mustafa Youssef Shilleh <104477522+shillehbean@users.noreply.github.com>
2022-12-16 22:49:18 -0600

    Create gyro_calibration_mpu6050.py

---
+++ gyro_calibration_mpu6050.py
@@ -0,0 +1,64 @@
+from imu import MPU6050
+from machine import Pin, I2C
+import time
+from time import sleep
+
+i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
+imu = MPU6050(i2c)
+t0 = time.time()
+# Wait for MPU to Settle
+settling_time = 4
+print('Settling MPU for %d seconds' % settling_time)
+time.sleep(4)
+print('MPU is Done Settling')
+
+
+def get_gyro():
+    gx=imu.gyro.x
+    gy=imu.gyro.y
+    gz=imu.gyro.z
+    return gx, gy, gz
+    
+
+def gyro_calibration(calibration_time=10):
+    """
+        Description: This is a function to get the offset values
+            for gyro calibration for mpu6050.
+        
+        Parameters:
+        
+        calibration_time[int]: Time in seconds you want to calibrate
+            mpu6050. The longer the time the more accurate the
+            calibration
+    
+        Outputs: Array with offsets pertaining to three axes of
+            rotation [offset_gx, offset_gy, offset_gz]. Add these
+            offsets to your sensor readins later on for more
+            accurate readings!
+    """
+    print('--' * 25)
+    print('Beginning Gyro Calibration - Do not move the MPU6050')
+    
+    # placeholder for the average of tuples in mpu_gyro_array
+    offsets = [0, 0, 0]
+    # placeholder for number of calculations we get from the mpu
+    num_of_points = 0
+    
+    # We get the current time and add the calibration time
+    end_loop_time = time.time() + calibration_time
+    # We end the loop once the calibration time has passed
+    while end_loop_time > time.time():
+        num_of_points += 1
+        (gx, gy, gz) = get_gyro()
+        offsets[0] += gx
+        offsets[1] += gy
+        offsets[2] += gz
+        
+        # This is just to show you its still calibrating :)
+        if num_of_points % 100 == 0:
+            print('Still Calibrating Gyro... %d points so far' % num_of_points)
+        
+    print('Calibration for Gyro is Complete! %d points total' % num_of_points)
+    offsets = [i/num_of_points for i in offsets] # we divide by the length to get the mean
+    return offsets
+  
