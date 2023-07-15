91e435c0c933d5ff5ba17563cf5198cc7d62b547
Mahmood Mustafa Youssef Shilleh <104477522+shillehbean@users.noreply.github.com>
2023-07-14 19:45:23 -0500

    Create ap_mode_part1.py

---
+++ ap_mode_part1.py
@@ -0,0 +1,49 @@
+import network
+import time
+import socket
+
+
+def web_page():
+  html = """<html><head><meta name="viewport" content="width=device-width, initial-scale=1"></head>
+            <body><h1>Hello World</h1></body></html>
+         """
+  return html
+
+# if you do not see the network you may have to power cycle
+# unplug your pico w for 10 seconds and plug it in again
+def ap_mode(ssid, password):
+    """
+        Description: This is a function to activate AP mode
+        
+        Parameters:
+        
+        ssid[str]: The name of your internet connection
+        password[str]: Password for your internet connection
+        
+        Returns: Nada
+    """
+    # Just making our internet connection
+    ap = network.WLAN(network.AP_IF)
+    ap.config(essid=ssid, password=password)
+    ap.active(True)
+    
+    while ap.active() == False:
+        pass
+    print('AP Mode Is Active, You can Now Connect')
+    print('IP Address To Connect to:: ' + ap.ifconfig()[0])
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #creating socket object
+    s.bind(('', 80))
+    s.listen(5)
+
+    while True:
+      conn, addr = s.accept()
+      print('Got a connection from %s' % str(addr))
+      request = conn.recv(1024)
+      print('Content = %s' % str(request))
+      response = web_page()
+      conn.send(response)
+      conn.close()
+      
+ap_mode('NAME',
+        'PASSWORD')
